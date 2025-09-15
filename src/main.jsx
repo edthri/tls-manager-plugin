de220ee5538ac945fcbaa62122737578817f86af
Andrei Haiducu <andressh11@gmail.com>
2025-09-15 13:07:42 +0300

    init

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { StyledEngineProvider } from '@mui/material/styles'
+import GlobalStyles from '@mui/material/GlobalStyles'
+import CssBaseline from '@mui/material/CssBaseline'
+import App from './App'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <StyledEngineProvider enableCssLayer>
+      <GlobalStyles styles="@layer theme, base, mui, components, utilities;" />
+      <CssBaseline />
+      <App />
+    </StyledEngineProvider>
+  </React.StrictMode>
+)
