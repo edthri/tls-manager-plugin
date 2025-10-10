8c1347349fadeea5b028f1f127e3d53b75a1a5c2
Kaur Palang <kaur.palang@brightcodecompany.com>
2025-10-10 15:20:01 +0300

    Add initial TLS to client-mode TCP Sender

--- server/src/main/java/org/openintegrationengine/tlsmanager/server/connectorconfig/TLSTcpConfiguration.java
+++ server/src/main/java/org/openintegrationengine/tlsmanager/server/connectorconfig/TLSTcpConfiguration.java
@@ -1,6 +1,55 @@
 package org.openintegrationengine.tlsmanager.server.connectorconfig;
 
 import com.mirth.connect.connectors.tcp.DefaultTcpConfiguration;
+import com.mirth.connect.connectors.tcp.StateAwareSocket;
+import com.mirth.connect.connectors.tcp.TcpDispatcher;
+import com.mirth.connect.donkey.server.channel.Connector;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.openintegrationengine.tlsmanager.server.SocketFactoryService;
+import org.openintegrationengine.tlsmanager.server.TLSServicePlugin;
+import org.openintegrationengine.tlsmanager.server.io.StateAwareTLSSocket;
+import org.openintegrationengine.tlsmanager.shared.properties.TLSConnectorProperties;
+
+import java.net.Socket;
 
 public class TLSTcpConfiguration extends DefaultTcpConfiguration {
+
+    private final SocketFactoryService socketFactoryService;
+
+    private TLSConnectorProperties tlsConnectorProperties;
+
+    private SSLConnectionSocketFactory socketFactory;
+
+    public TLSTcpConfiguration() {
+        this(TLSServicePlugin.getPluginInstance().getSocketFactoryService());
+    }
+
+    public TLSTcpConfiguration(SocketFactoryService socketFactoryService) {
+        this.socketFactoryService = socketFactoryService;
+    }
+
+    @Override
+    public void configureConnectorDeploy(Connector connector) throws Exception {
+        var tcpDispatcher = (TcpDispatcher) connector;
+
+        this.tlsConnectorProperties = tcpDispatcher.getConnectorProperties().getPluginProperties()
+            .stream()
+            .filter(TLSConnectorProperties.class::isInstance)
+            .findFirst()
+            .map(TLSConnectorProperties.class::cast)
+            .orElse(null);
+
+        if (tlsConnectorProperties != null && tlsConnectorProperties.isTlsManagerEnabled()) {
+            socketFactory = socketFactoryService.getConnectorSocketFactory(tcpDispatcher, tlsConnectorProperties);
+        }
+    }
+
+    @Override
+    public Socket createSocket() {
+        if (tlsConnectorProperties == null || !tlsConnectorProperties.isTlsManagerEnabled()) {
+            return new StateAwareSocket();
+        } else {
+            return new StateAwareTLSSocket(socketFactory);
+        }
+    }
 }
---
+++ server/src/main/java/org/openintegrationengine/tlsmanager/server/io/StateAwareTLSSocket.java
@@ -0,0 +1,74 @@
+package org.openintegrationengine.tlsmanager.server.io;
+
+import com.mirth.connect.connectors.tcp.StateAwareSocket;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+public class StateAwareTLSSocket extends StateAwareSocket {
+
+    private final SSLConnectionSocketFactory socketFactory;
+
+    private Socket sslSocket;
+
+    public StateAwareTLSSocket(SSLConnectionSocketFactory socketFactory) {
+        this.socketFactory = socketFactory;
+    }
+
+    @Override
+    public void connect(SocketAddress endpoint) throws IOException {
+        this.connect(endpoint, 0);
+    }
+
+    @Override
+    public void connect(SocketAddress endpoint, int timeout) throws IOException {
+        // Step 1: Perform the plain TCP connection first
+        super.connect(endpoint, timeout);
+
+        // Step 2: Layer TLS on top using createLayeredSocket
+        if (endpoint instanceof InetSocketAddress inet) {
+            String host = inet.getHostString();
+            int port = inet.getPort();
+
+            // createLayeredSocket() will internally call SSLSocketFactory.createSocket()
+            this.sslSocket = socketFactory.createLayeredSocket(this, host, port, null);
+        } else {
+            throw new IOException("Expected InetSocketAddress for TLS connection");
+        }
+    }
+
+    @Override
+    public InputStream getInputStream() throws IOException {
+        if (sslSocket != null) {
+            return sslSocket.getInputStream();
+        }
+        return super.getInputStream();
+    }
+
+    @Override
+    public OutputStream getOutputStream() throws IOException {
+        if (sslSocket != null) {
+            return sslSocket.getOutputStream();
+        }
+        return super.getOutputStream();
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (sslSocket != null) {
+            sslSocket.close();
+        } else {
+            super.close();
+        }
+    }
+
+    @Override
+    public boolean remoteSideHasClosed() throws IOException {
+        return super.remoteSideHasClosed();
+    }
+}
