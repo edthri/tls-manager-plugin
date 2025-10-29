4ca0da62a410a4bdcf44ccdcfce437266059667a
Kaur Palang <kaur.palang@brightcodecompany.com>
2025-10-29 16:16:55 +0200

    More Listener UI and properties

--- client/src/main/java/org/openintegrationengine/tlsmanager/client/panel/ListenerConnectorPropertiesPanel.java
+++ client/src/main/java/org/openintegrationengine/tlsmanager/client/panel/ListenerConnectorPropertiesPanel.java
@@ -2,19 +2,34 @@ package org.openintegrationengine.tlsmanager.client.panel;
 
 import com.mirth.connect.client.ui.AbstractConnectorPropertiesPanel;
 import com.mirth.connect.client.ui.Frame;
+import com.mirth.connect.client.ui.PlatformUI;
 import com.mirth.connect.client.ui.UIConstants;
+import com.mirth.connect.client.ui.components.MirthComboBox;
 import com.mirth.connect.client.ui.components.MirthRadioButton;
+import com.mirth.connect.client.ui.components.MirthTextField;
 import com.mirth.connect.donkey.model.channel.ConnectorPluginProperties;
 import com.mirth.connect.donkey.model.channel.ConnectorProperties;
 import com.mirth.connect.model.Connector;
 import net.miginfocom.swing.MigLayout;
+import org.openintegrationengine.tlsmanager.client.dialog.ItemPickerDialog;
+import org.openintegrationengine.tlsmanager.client.misc.DisplayTextEnumModeComboBoxRenderer;
+import org.openintegrationengine.tlsmanager.shared.models.RevocationMode;
+import org.openintegrationengine.tlsmanager.shared.models.SubjectDnValidationMode;
 import org.openintegrationengine.tlsmanager.shared.properties.TLSListenerProperties;
 
 import javax.swing.ButtonGroup;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Set;
+import java.util.function.BiConsumer;
 
 public class ListenerConnectorPropertiesPanel extends AbstractConnectorPropertiesPanel {
 
@@ -22,9 +37,38 @@ public class ListenerConnectorPropertiesPanel extends AbstractConnectorPropertie
     private MirthRadioButton managerEnabledRadioYes;
     private MirthRadioButton managerEnabledRadioNo;
 
+    private JLabel clientAuthLabel;
+    private MirthRadioButton clientAuthRadioNone;
+    private MirthRadioButton clientAuthRadioRequested;
+    private MirthRadioButton clientAuthRadioRequired;
+
+    // Trusted client certs picker
+
+    private JLabel subjectDnValidationLabel;
+    private MirthComboBox<SubjectDnValidationMode> subjectDnValidationModeComboBox;
+    private MirthTextField subjectDnValidationFilterTextField;
+
+    private JLabel crlModeLabel;
+    private MirthComboBox<RevocationMode> crlModeComboBox;
+
+    private JLabel ocspModeLabel;
+    private MirthComboBox<RevocationMode> ocspModeComboBox;
+
+    private JLabel protocolsLabel;
+    private JButton protocolsButton;
+    private JLabel protocolsText;
+
+    private JLabel ciphersLabel;
+    private JButton ciphersButton;
+    private JLabel ciphersText;
+
+    private final Frame parentFrame;
     private TLSListenerProperties properties;
 
     public ListenerConnectorPropertiesPanel() {
+        this.parentFrame = PlatformUI.MIRTH_FRAME;
+        this.properties = new TLSListenerProperties();
+
         initComponents();
         initLayout();
     }
@@ -75,14 +119,126 @@ public class ListenerConnectorPropertiesPanel extends AbstractConnectorPropertie
         managerEnabledRadioYes = new MirthRadioButton();
         managerEnabledRadioYes.setText("Yes");
         managerEnabledRadioYes.setBackground(Color.white);
-        //managerEnabledRadioYes.addActionListener(e -> handleManagerEnabledButton(true));
+        managerEnabledRadioYes.addActionListener(e -> handleManagerEnabledButton(true));
         managerEnabledButtonGroup.add(managerEnabledRadioYes);
 
         managerEnabledRadioNo = new MirthRadioButton();
         managerEnabledRadioNo.setText("No");
         managerEnabledRadioNo.setBackground(Color.white);
-        //managerEnabledRadioNo.addActionListener(e -> handleManagerEnabledButton(false));
+        managerEnabledRadioNo.addActionListener(e -> handleManagerEnabledButton(false));
         managerEnabledButtonGroup.add(managerEnabledRadioNo);
+
+        clientAuthLabel = new JLabel("Client Authentication Mode");
+
+        var clientAuthModeButtonGroup = new ButtonGroup();
+        clientAuthRadioNone = new MirthRadioButton();
+        clientAuthRadioNone.setText("None");
+        clientAuthRadioNone.setBackground(Color.white);
+        clientAuthModeButtonGroup.add(clientAuthRadioNone);
+
+        clientAuthRadioRequested = new MirthRadioButton();
+        clientAuthRadioRequested.setText("Requested");
+        clientAuthRadioRequested.setBackground(Color.white);
+        clientAuthModeButtonGroup.add(clientAuthRadioRequested);
+
+        clientAuthRadioRequired = new MirthRadioButton();
+        clientAuthRadioRequired.setText("Required");
+        clientAuthRadioRequired.setBackground(Color.white);
+        clientAuthModeButtonGroup.add(clientAuthRadioRequired);
+
+        var comboBoxRenderer = new DisplayTextEnumModeComboBoxRenderer();
+
+        var subjectDnValidationModeModel = new SubjectDnValidationMode[]{
+            SubjectDnValidationMode.NONE,
+            SubjectDnValidationMode.PARTIAL,
+            SubjectDnValidationMode.EXACT,
+        };
+
+        subjectDnValidationLabel = new JLabel("Subject DN Validation Mode:");
+        subjectDnValidationModeComboBox = new MirthComboBox<>();
+        subjectDnValidationModeComboBox.setRenderer(comboBoxRenderer);
+        subjectDnValidationModeComboBox.setModel(new DefaultComboBoxModel<>(subjectDnValidationModeModel));
+        subjectDnValidationModeComboBox.addActionListener(evt -> handleSubjectDnValidationModeChange());
+
+        subjectDnValidationFilterTextField = new MirthTextField();
+        subjectDnValidationFilterTextField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                properties.setSubjectDnValidationFilter(subjectDnValidationFilterTextField.getText());
+            }
+        });
+
+        var revocationModeModel = new RevocationMode[]{
+            RevocationMode.DISABLED,
+            RevocationMode.SOFT_FAIL,
+            RevocationMode.HARD_FAIL
+        };
+
+        crlModeLabel = new JLabel("CRL Mode:");
+        crlModeComboBox = new MirthComboBox<>();
+        crlModeComboBox.setRenderer(comboBoxRenderer);
+        crlModeComboBox.setModel(new DefaultComboBoxModel<>(revocationModeModel));
+        crlModeComboBox.addActionListener(evt -> handleCrlModeChange());
+
+        ocspModeLabel = new JLabel("OCSP Mode:");
+        ocspModeComboBox = new MirthComboBox<>();
+        ocspModeComboBox.setRenderer(comboBoxRenderer);
+        ocspModeComboBox.setModel(new DefaultComboBoxModel<>(revocationModeModel));
+        ocspModeComboBox.addActionListener(evt -> handleOcspModeChange());
+
+        protocolsLabel = new JLabel("Enabled Protocols:");
+        protocolsButton = new JButton(wrenchIcon);
+        protocolsButton.addActionListener(e -> {
+            BiConsumer<Boolean, Set<String>> completionConsumer = (trustDefaultProtocols, selectedProtocols) -> {
+                properties.setUseServerDefaultProtocols(trustDefaultProtocols);
+                if (trustDefaultProtocols) {
+                    properties.setUsedProtocols(Collections.emptySet());
+                } else {
+                    properties.setUsedProtocols(selectedProtocols);
+                }
+
+                redrawState();
+                PlatformUI.MIRTH_FRAME.setSaveEnabled(true);
+            };
+
+            new ItemPickerDialog(
+                PlatformUI.MIRTH_FRAME,
+                "Protocols Picker",
+                Set.of(PlatformUI.SERVER_HTTPS_SUPPORTED_PROTOCOLS),
+                properties.getUsedProtocols(),
+                properties.isUseServerDefaultProtocols(),
+                "[Server default]",
+                completionConsumer
+            );
+        });
+        protocolsText = new JLabel("Server default: TLSv4.6");
+
+        ciphersLabel = new JLabel("Enabled Ciphers:");
+        ciphersButton = new JButton(wrenchIcon);
+        ciphersButton.addActionListener(e -> {
+            BiConsumer<Boolean, Set<String>> completionConsumer = (trustDefaultCiphers, selectedCiphers) -> {
+                properties.setUseServerDefaultCiphers(trustDefaultCiphers);
+                if (trustDefaultCiphers) {
+                    properties.setUsedCiphers(Collections.emptySet());
+                } else {
+                    properties.setUsedCiphers(selectedCiphers);
+                }
+
+                redrawState();
+                PlatformUI.MIRTH_FRAME.setSaveEnabled(true);
+            };
+
+            new ItemPickerDialog(
+                PlatformUI.MIRTH_FRAME,
+                "Ciphers Picker",
+                Set.of(PlatformUI.SERVER_HTTPS_SUPPORTED_CIPHER_SUITES),
+                properties.getUsedCiphers(),
+                properties.isUseServerDefaultCiphers(),
+                "[Server default]",
+                completionConsumer
+            );
+        });
+        ciphersText = new JLabel("Server default: 22 enabled");
     }
 
     private void initLayout() {
@@ -91,5 +247,100 @@ public class ListenerConnectorPropertiesPanel extends AbstractConnectorPropertie
         add(managerEnabledLabel, "newline, right");
         add(managerEnabledRadioYes, "split");
         add(managerEnabledRadioNo);
+
+        add(clientAuthLabel, "newline, right");
+        add(clientAuthRadioNone, "split");
+        add(clientAuthRadioRequested, "split");
+        add(clientAuthRadioRequired);
+
+        add(subjectDnValidationLabel, "newline, right");
+        add(subjectDnValidationModeComboBox, "split");
+        add(subjectDnValidationFilterTextField, "w 168!");
+
+        add(crlModeLabel, "newline, right");
+        add(crlModeComboBox);
+
+        add(ocspModeLabel, "newline, right");
+        add(ocspModeComboBox);
+
+        add(protocolsLabel, "newline, right");
+        add(protocolsButton, "h 22!, w 22!, split");
+        add(protocolsText);
+
+        add(ciphersLabel, "newline, right");
+        add(ciphersButton, "h 22!, w 22!, split");
+        add(ciphersText);
+    }
+
+    private void handleSubjectDnValidationModeChange() {
+        if (subjectDnValidationModeComboBox.getSelectedItem() instanceof SubjectDnValidationMode validationMode) {
+            properties.setSubjectDnValidationMode(validationMode);
+            redrawState();
+        }
+    }
+
+    private void handleCrlModeChange() {
+        if (crlModeComboBox.getSelectedItem() instanceof RevocationMode revocationMode) {
+            properties.setCrlMode(revocationMode);
+        }
+    }
+
+    private void handleOcspModeChange() {
+        if (ocspModeComboBox.getSelectedItem() instanceof RevocationMode revocationMode) {
+            properties.setOcspMode(revocationMode);
+        }
+    }
+
+    private void handleManagerEnabledButton(boolean managerEnabled) {
+        properties.setTlsManagerEnabled(managerEnabled);
+
+        subjectDnValidationLabel.setEnabled(managerEnabled);
+        subjectDnValidationModeComboBox.setEnabled(managerEnabled);
+        subjectDnValidationFilterTextField.setEnabled(managerEnabled);
+
+        crlModeLabel.setEnabled(managerEnabled);
+        crlModeComboBox.setEnabled(managerEnabled);
+
+        ocspModeLabel.setEnabled(managerEnabled);
+        ocspModeComboBox.setEnabled(managerEnabled);
+
+        protocolsLabel.setEnabled(managerEnabled);
+        protocolsButton.setEnabled(managerEnabled);
+        protocolsText.setEnabled(managerEnabled);
+
+        ciphersLabel.setEnabled(managerEnabled);
+        ciphersButton.setEnabled(managerEnabled);
+        ciphersText.setEnabled(managerEnabled);
+    }
+
+    private void redrawState() {
+        if (properties.isTlsManagerEnabled()) {
+            managerEnabledRadioYes.setSelected(true);
+        } else {
+            managerEnabledRadioNo.setSelected(true);
+        }
+
+        subjectDnValidationModeComboBox.setSelectedItem(properties.getSubjectDnValidationMode());
+        subjectDnValidationFilterTextField.setEnabled(properties.getSubjectDnValidationMode() != SubjectDnValidationMode.NONE);
+        subjectDnValidationFilterTextField.setText(properties.getSubjectDnValidationFilter());
+
+        crlModeComboBox.setSelectedItem(properties.getCrlMode());
+        ocspModeComboBox.setSelectedItem(properties.getOcspMode());
+
+        var protocolsString = properties.isUseServerDefaultProtocols()
+            ? "Server default: %s".formatted(Arrays.toString(PlatformUI.HTTPS_PROTOCOLS))
+            : "%d selected".formatted(properties.getUsedProtocols().size());
+
+        protocolsText.setText(protocolsString);
+
+        var ciphersString = properties.isUseServerDefaultCiphers()
+            ? "Server default: %d selected".formatted(PlatformUI.HTTPS_CIPHER_SUITES.length)
+            : "%d selected".formatted(properties.getUsedCiphers().size());
+
+        ciphersText.setText(ciphersString);
+    }
+
+    private void fetchData() {
+
     }
 }
--- shared/src/main/java/org/openintegrationengine/tlsmanager/shared/properties/TLSListenerProperties.java
+++ shared/src/main/java/org/openintegrationengine/tlsmanager/shared/properties/TLSListenerProperties.java
@@ -6,8 +6,12 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import org.openintegrationengine.tlsmanager.shared.TLSPluginConstants;
+import org.openintegrationengine.tlsmanager.shared.models.RevocationMode;
+import org.openintegrationengine.tlsmanager.shared.models.SubjectDnValidationMode;
 
+import java.util.Collections;
 import java.util.Map;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -17,12 +21,51 @@ public class TLSListenerProperties extends ConnectorPluginProperties {
 
     private boolean isTlsManagerEnabled;
 
+    private SubjectDnValidationMode subjectDnValidationMode;
+    private String subjectDnValidationFilter;
+
+    // Certificate revocation modes
+    private RevocationMode crlMode;
+    private RevocationMode ocspMode;
+
+    // Protocols
+    private boolean isUseServerDefaultProtocols;
+    private Set<String> usedProtocols;
+
+    // Ciphers
+    private boolean isUseServerDefaultCiphers;
+    private Set<String> usedCiphers;
+
     public TLSListenerProperties() {
         isTlsManagerEnabled = false;
+
+        subjectDnValidationMode = SubjectDnValidationMode.NONE;
+        subjectDnValidationFilter = null;
+
+        crlMode = RevocationMode.HARD_FAIL;
+        ocspMode = RevocationMode.HARD_FAIL;
+
+        isUseServerDefaultProtocols = true;
+        usedProtocols = Collections.emptySet();
+
+        isUseServerDefaultCiphers = true;
+        usedCiphers = Collections.emptySet();
     }
 
-    public TLSListenerProperties(TLSListenerProperties properties) {
-        isTlsManagerEnabled = properties.isTlsManagerEnabled();
+    public TLSListenerProperties(TLSListenerProperties props) {
+        isTlsManagerEnabled = props.isTlsManagerEnabled();
+
+        subjectDnValidationMode = props.getSubjectDnValidationMode();
+        subjectDnValidationFilter = props.getSubjectDnValidationFilter();
+
+        crlMode = props.getCrlMode();
+        ocspMode = props.getOcspMode();
+
+        isUseServerDefaultProtocols = props.isUseServerDefaultProtocols();
+        usedProtocols = props.getUsedProtocols();
+
+        isUseServerDefaultCiphers = props.isUseServerDefaultCiphers();
+        usedCiphers = props.getUsedCiphers();
     }
 
     @Override
