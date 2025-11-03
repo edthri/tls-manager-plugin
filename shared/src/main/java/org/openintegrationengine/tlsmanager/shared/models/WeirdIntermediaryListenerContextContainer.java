1523b9d1787eca753bff720d63470d22ef69bf58
Kaur Palang <kaur.palang@brightcodecompany.com>
2025-11-03 18:04:55 +0200

    Commit leftovers, too

--- shared/src/main/java/org/openintegrationengine/tlsmanager/shared/models/WeirdIntermediaryListenerContextContainer.java
+++ shared/src/main/java/org/openintegrationengine/tlsmanager/shared/models/WeirdIntermediaryListenerContextContainer.java
@@ -1,11 +1,13 @@
 package org.openintegrationengine.tlsmanager.shared.models;
 
 import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
 import java.security.KeyStore;
 
 public record WeirdIntermediaryListenerContextContainer(
     String[] protocols,
     String[] ciphers,
     HostnameVerifier hostnameVerifier,
-    KeyStore keyStore
+    KeyStore keyStore,
+    SSLContext sslContext
 ) {}
