d118fa494dac684beed647efa59c49960d15fed1
Kaur Palang <kaur.palang@brightcodecompany.com>
2025-12-22 16:24:07 +0200

    Stop using inheritance for the properties
    This does not seem to play nice with the `EqualsBuilder.reflectionEquals()` used to compare connector properties.
    
    https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExcmZjaHk4NHNqNDNwMG9oZGE0czgwOWc3eGJ1dHM4a21mbWs2OWRpNCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/YVPwi7L2izTJS/giphy.gif

--- shared/src/main/java/org/openintegrationengine/tlsmanager/shared/properties/TLSConnectorProperties.java
+++ shared/src/main/java/org/openintegrationengine/tlsmanager/shared/properties/TLSConnectorProperties.java
@@ -8,16 +8,40 @@ import lombok.Setter;
 import lombok.ToString;
 import org.openintegrationengine.tlsmanager.shared.TLSPluginConstants;
 import org.openintegrationengine.tlsmanager.shared.models.ClientAuthMode;
+import org.openintegrationengine.tlsmanager.shared.models.RevocationMode;
+import org.openintegrationengine.tlsmanager.shared.models.SubjectDnValidationMode;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @Setter
 @EqualsAndHashCode(callSuper = false)
 @AllArgsConstructor
-@ToString(callSuper = true)
-public class TLSConnectorProperties extends AbstractTLSConnectorProperties {
+@ToString
+public class TLSConnectorProperties extends ConnectorPluginProperties {
+
+    protected boolean isTlsManagerEnabled;
+
+    protected boolean trustSystemTruststore;
+    protected Set<String> trustedServerCertificates;
+
+    // Certificate revocation modes
+    protected RevocationMode crlMode;
+    protected RevocationMode ocspMode;
+
+    protected SubjectDnValidationMode subjectDnValidationMode;
+    protected String subjectDnValidationFilter;
+
+    // Protocols
+    protected boolean isUseServerDefaultProtocols;
+    protected Set<String> usedProtocols;
+
+    // Ciphers
+    protected boolean isUseServerDefaultCiphers;
+    protected Set<String> usedCiphers;
 
     // Server mode properties
     private String serverCertificateAlias;
@@ -29,7 +53,22 @@ public class TLSConnectorProperties extends AbstractTLSConnectorProperties {
     private String clientCertificateAlias;
 
     public TLSConnectorProperties() {
-        super();
+        isTlsManagerEnabled = false;
+
+        trustSystemTruststore = true;
+        trustedServerCertificates = Collections.emptySet();
+
+        subjectDnValidationMode = SubjectDnValidationMode.NONE;
+        subjectDnValidationFilter = null;
+
+        crlMode = RevocationMode.HARD_FAIL;
+        ocspMode = RevocationMode.HARD_FAIL;
+
+        isUseServerDefaultProtocols = true;
+        usedProtocols = Collections.emptySet();
+
+        isUseServerDefaultCiphers = true;
+        usedCiphers = Collections.emptySet();
 
         // Server mode properties
         serverCertificateAlias = null;
@@ -42,7 +81,34 @@ public class TLSConnectorProperties extends AbstractTLSConnectorProperties {
     }
 
     public TLSConnectorProperties(TLSConnectorProperties props) {
-        super(props);
+        isTlsManagerEnabled = props.isTlsManagerEnabled();
+
+        trustSystemTruststore = props.isTrustSystemTruststore();
+        trustedServerCertificates = Objects.requireNonNullElse(
+            props.getTrustedServerCertificates(),
+            Collections.emptySet()
+        );
+
+        subjectDnValidationMode = Objects.requireNonNullElse(
+            props.getSubjectDnValidationMode(),
+            SubjectDnValidationMode.NONE
+        );
+        subjectDnValidationFilter = props.getSubjectDnValidationFilter();
+
+        crlMode = Objects.requireNonNullElse(props.getCrlMode(), RevocationMode.HARD_FAIL);
+        ocspMode = Objects.requireNonNullElse(props.getOcspMode(), RevocationMode.HARD_FAIL);
+
+        isUseServerDefaultProtocols = props.isUseServerDefaultProtocols();
+        usedProtocols = Objects.requireNonNullElse(
+            props.getUsedProtocols(),
+            Collections.emptySet()
+        );
+
+        isUseServerDefaultCiphers = props.isUseServerDefaultCiphers();
+        usedCiphers = Objects.requireNonNullElse(
+            props.getUsedCiphers(),
+            Collections.emptySet()
+        );
 
         // Server mode properties
         serverCertificateAlias = props.getServerCertificateAlias();
