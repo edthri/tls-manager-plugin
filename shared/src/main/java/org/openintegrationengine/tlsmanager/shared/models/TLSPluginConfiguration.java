b5808b93a120d4d6a6b30ce6b534cd0203396417
Kaur Palang <kaur.palang@brightcodecompany.com>
2025-12-25 19:05:51 +0200

    Add correct SPDX license headers to files in shared directory

--- shared/src/main/java/org/openintegrationengine/tlsmanager/shared/models/TLSPluginConfiguration.java
+++ shared/src/main/java/org/openintegrationengine/tlsmanager/shared/models/TLSPluginConfiguration.java
@@ -1,3 +1,8 @@
+/*
+ * SPDX-License-Identifier: MPL-2.0
+ * Copyright (c) 2025 NovaMap Health Limited <https://novamap.health>
+ */
+
 package org.openintegrationengine.tlsmanager.shared.models;
 
 import lombok.extern.slf4j.Slf4j;
